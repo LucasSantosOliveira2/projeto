4b8b9187ae57c0c74a9aa0bc4df83d6f50aca5b7
Tales Soares <talesschifelbeinsoares@gmail.com>
2023-11-27 16:39:32 -0300

    persistencia do login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -2,7 +2,6 @@ import * as S from "./styles";
 import Arrow from "../../components/Images/Arrow";
 import Google from "../../components/Images/GoogleIcon";
 import { useGoogleLogin } from '@react-oauth/google';
-import axios from "axios";
 import { useNavigate } from 'react-router-dom';
 import { useUser } from "../../components/UserContext";
 
@@ -10,33 +9,22 @@ export const SignIn = () => {
     const navigate = useNavigate();
     const { setUser } = useUser();
 
-
     const googleLogin = useGoogleLogin({
         onSuccess: async (tokenResponse) => {
             console.log(tokenResponse);
 
             try {
-                const userInfo = await axios.get(
+                const userInfo = await fetch(
                     'https://www.googleapis.com/oauth2/v3/userinfo',
-<<<<<<< HEAD
                     { headers: { Authorization: `Bearer ${tokenResponse.access_token}` } }
                 ).then(response => response.json());
 
                 console.log(userInfo);
 
-                // Salvar informações relevantes no localStorage
-                localStorage.setItem('token', tokenResponse.access_token);
-                localStorage.setItem('userInfo', JSON.stringify(userInfo));
-
-                // Configurar o estado do usuário
                 setUser(userInfo);
-=======
-                    { headers: { Authorization: `Bearer ${tokenResponse.access_token}` } },
-                );
->>>>>>> 315a8a6777e1f3b03aca8bfe60393b7240daf41c
 
-                console.log(userInfo.data);
-                setUser(userInfo.data);
+                localStorage.setItem('token', tokenResponse.access_token);
+
                 navigate('/profile');
             } catch (error) {
                 console.error('Erro ao obter informações do usuário:', error);
@@ -51,7 +39,7 @@ export const SignIn = () => {
                 <S.StyledLink to="/">
                     <S.ButtonBack>
                         <Arrow />
-                        <S.TextDashborder>Voltar</S.TextDashborder >
+                        <S.TextDashborder>Voltar</S.TextDashborder>
                     </S.ButtonBack>
                 </S.StyledLink>
                 <S.Headline>
@@ -61,7 +49,7 @@ export const SignIn = () => {
                 <S.ButtonGoogle onClick={() => googleLogin()}>
                     <Google />Entrar com Google
                 </S.ButtonGoogle>
-            </S.Container >
-        </S.Wrapper >
-    )
-}
+            </S.Container>
+        </S.Wrapper>
+    );
+};
