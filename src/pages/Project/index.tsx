26c53c14f332fe2b44e1fc9036ad47aa45f026bd
Matheus <matheus_orbotafogo@hotmail.com>
2023-12-01 14:04:19 -0300

    Modificação do objeto recebido

--- src/pages/Project/index.tsx
+++ src/pages/Project/index.tsx
@@ -1,49 +1,63 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import * as S from "./styles";
 import { Sidebar } from "../../components/Sidebar";
 import { HeaderDashboard } from "../../components/HeaderDashboard";
 import { ProjectCard } from "../../components/ProjectCard";
 
+interface project {
+  nomeProjeto: string;
+  idProjeto: number;
+}
+
 export const Project = () => {
-    const [projects, setProjects] = useState([]);
-    
-    const userInfo = window.localStorage.getItem('userInfo');
-    const userEmail = userInfo ? JSON.parse(userInfo).email : '';
+  const [projects, setProjects] = useState<project[]>([]);
 
-    useEffect(() => {
-        fetch('http://localhost:8080/project', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email: userEmail
-            }),
-        })
-        .then(response => response.json())
-        .then(data => {
-            setProjects(data);
-        })
-        .catch(error => {
-            console.error(error);
+  const sup: { nomeProjeto: any; idProjeto: any }[] = [];
+
+  const userInfo = window.localStorage.getItem("userInfo");
+  const userEmail = userInfo ? JSON.parse(userInfo).email : "";
+
+  useEffect(() => {
+    fetch("http://localhost:8080/project", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: userEmail,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        data.forEach((element: { nomeProjeto: any; idProjeto: any }) => {
+          sup.push(element);
         });
-    }, [userEmail]);
+        setProjects(sup);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [userEmail]);
 
-    return (
-        <S.Wrapper>
-            <S.SidebarContainer>
-                <Sidebar />
-                <HeaderDashboard />
-            </S.SidebarContainer>
+  return (
+    <S.Wrapper>
+      <S.SidebarContainer>
+        <Sidebar />
+        <HeaderDashboard />
+      </S.SidebarContainer>
 
-            <S.ContentContainer>
-                <S.InfoContainer>
-                    <S.Title>Projetos</S.Title>
-                    {projects.map((project, index) => (
-                        <ProjectCard key={index} title={project} number={(index + 1).toString()} />
-                    ))}
-                </S.InfoContainer>
-            </S.ContentContainer>
-        </S.Wrapper>
-    );
+      <S.ContentContainer>
+        <S.InfoContainer>
+          <S.Title>Projetos</S.Title>
+          {projects.map((project, index) => (
+            <ProjectCard
+              key={index}
+              title={project.nomeProjeto}
+              number={(index + 1).toString()}
+            />
+          ))}
+        </S.InfoContainer>
+      </S.ContentContainer>
+    </S.Wrapper>
+  );
 };
