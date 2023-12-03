8bbdfa650a2f26dd8b2a8967db131c036b7a60f7
Tales Soares <talesschifelbeinsoares@gmail.com>
2023-12-03 14:50:51 -0300

    correcao dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -5,35 +5,35 @@ import { Graphic } from "../../components/Graphic";
 import { PredominantSentiment } from "../../components/PredominantSentiment";
 import { useState, useEffect } from "react";
 
-
 export const Dashboard = () => {
-
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:8080/api/sentimentos/mockGrafico', {
-      method: 'GET',
+    fetch("http://localhost:8080/api/sentimentos/mockGrafico", {
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     })
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         setData(data);
       })
-      .catch(error => {
-        console.error('Erro ao obter dados:', error);
+      .catch((error) => {
+        console.error("Erro ao obter dados:", error);
       });
   }, []);
 
-
   const formattedData: [string, number][] = data
-    ? Object.entries(data).map(([emoção, porcentagem]) => [emoção, Number(porcentagem)])
+    ? Object.entries(data).map(([emoção, porcentagem]) => [
+        emoção,
+        Number(porcentagem),
+      ])
     : [];
 
   const combinedData = [["Emoção", "Porcentagem"], ...formattedData];
 
-  console.log(combinedData)
+  console.log(combinedData);
   const mostDominantEmotion: [string, number] = formattedData.reduce(
     (prev: [string, number], current: [string, number]) =>
       prev[1] > current[1] ? prev : current,
@@ -41,7 +41,11 @@ export const Dashboard = () => {
   );
 
   const [dominantEmotion, dominantPercentage] = mostDominantEmotion;
-  console.log("Resultado da emoção dominante", dominantEmotion, dominantPercentage);
+  console.log(
+    "Resultado da emoção dominante",
+    dominantEmotion,
+    dominantPercentage
+  );
 
   return (
     <S.Wrapper>
@@ -51,13 +55,21 @@ export const Dashboard = () => {
       </S.SidebarContainer>
       <S.ContentContainer>
         <S.ContainerInfo>
-          <PredominantSentiment emotion={dominantEmotion} percentage={dominantPercentage} />
-          <S.GraphicWrapper>
-            <S.Title>Nuvem de Palavras</S.Title>
-          </S.GraphicWrapper>
+          <PredominantSentiment
+            emotion={dominantEmotion}
+            percentage={dominantPercentage}
+          />
+          <Graphic />
         </S.ContainerInfo>
-        <Graphic />
+        <S.ContainerInfo>
+          <S.GraphicWrapper>
+            <S.Title>Polaridade</S.Title>
+          </S.GraphicWrapper>
+          <S.GraphicWrapper2>
+            <S.Title>Nuvem de Palavras</S.Title>
+          </S.GraphicWrapper2>
+        </S.ContainerInfo>
       </S.ContentContainer>
-    </S.Wrapper >
+    </S.Wrapper>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -40,6 +40,18 @@ export const GraphicWrapper = styled.div`
   flex-direction: column;
 `
 
+export const GraphicWrapper2 = styled.div`
+  max-width: 778px;
+  width: 100%;
+  height: 345px;
+  border-radius: 20px;
+  background: #111C44;
+  display: flex;
+  padding: 20px;
+  align-items: center;
+  flex-direction: column;
+`
+
 export const Title = styled.h1`
   font-size: 1.2rem;
   font-weight: 700;
