3ab49a153d907ddb0a1d04777c5a297b709e1e6e
Matheus <matheus_orbotafogo@hotmail.com>
2023-12-06 23:11:05 -0300

    Teste para cloud

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,66 +1,67 @@
 import * as S from "./styles";
 import Arrow from "../../components/Images/Arrow";
 import Google from "../../components/Images/GoogleIcon";
-import { useGoogleLogin } from '@react-oauth/google';
-import { useNavigate } from 'react-router-dom';
+import { useGoogleLogin } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
 import { useUser } from "../../components/UserContext";
-import axios from 'axios';
+import axios from "axios";
 
 export const SignIn = () => {
-    const navigate = useNavigate();
-    const { setUser } = useUser();
+  const navigate = useNavigate();
+  const { setUser } = useUser();
 
-    const googleLogin = useGoogleLogin({
-        onSuccess: async (tokenResponse) => {
+  const googleLogin = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      try {
+        const userInfoResponse = await axios.get(
+          "https://www.googleapis.com/oauth2/v3/userinfo",
+          {
+            headers: { Authorization: `Bearer ${tokenResponse.access_token}` },
+          }
+        );
 
-            try {
-                const userInfoResponse = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo', {
-                    headers: { Authorization: `Bearer ${tokenResponse.access_token}` },
-                });
+        const userInfo = userInfoResponse.data;
 
-                const userInfo = userInfoResponse.data;
+        fetch("http://35.209.202.3:8080/auth/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: userInfo.email,
+            name: userInfo.name,
+          }),
+        });
+        setUser(userInfo);
 
-                fetch('http://localhost:8080/auth/login', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        email: userInfo.email,
-                        name: userInfo.name,
-                    }),
+        window.localStorage.setItem("userInfo", JSON.stringify(userInfo));
 
-                })
-                setUser(userInfo);
+        navigate("/profile");
+      } catch (error) {
+        console.error("Erro ao obter informações do usuário:", error);
+      }
+    },
+    onError: (errorResponse) => console.log(errorResponse),
+  });
 
-                window.localStorage.setItem('userInfo', JSON.stringify(userInfo));
-
-                navigate('/profile');
-            } catch (error) {
-                console.error('Erro ao obter informações do usuário:', error);
-            }
-        },
-        onError: (errorResponse) => console.log(errorResponse),
-    });
-
-
-    return (
-        <S.Wrapper>
-            <S.Container>
-                <S.StyledLink to="/">
-                    <S.ButtonBack>
-                        <Arrow />
-                        <S.TextDashborder>Voltar</S.TextDashborder>
-                    </S.ButtonBack>
-                </S.StyledLink>
-                <S.Headline>
-                    <S.Title>Entrar</S.Title>
-                    <S.Text>Entre com sua conta Google</S.Text>
-                </S.Headline>
-                <S.ButtonGoogle onClick={() => googleLogin()}>
-                    <Google />Entrar com Google
-                </S.ButtonGoogle>
-            </S.Container>
-        </S.Wrapper>
-    );
+  return (
+    <S.Wrapper>
+      <S.Container>
+        <S.StyledLink to="/">
+          <S.ButtonBack>
+            <Arrow />
+            <S.TextDashborder>Voltar</S.TextDashborder>
+          </S.ButtonBack>
+        </S.StyledLink>
+        <S.Headline>
+          <S.Title>Entrar</S.Title>
+          <S.Text>Entre com sua conta Google</S.Text>
+        </S.Headline>
+        <S.ButtonGoogle onClick={() => googleLogin()}>
+          <Google />
+          Entrar com Google
+        </S.ButtonGoogle>
+      </S.Container>
+    </S.Wrapper>
+  );
 };
